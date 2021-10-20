b756be416d94c23f5a53518adcadee8782125a79
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2021-10-20 22:31:31 +0200

    Bootstrap the config

--- machines/brausefrosch/default.nix
+++ machines/brausefrosch/default.nix
@@ -1,9 +1,74 @@
 { mkMachine, ... }:
 
-mkMachine {} ({ pkgs, ... }: {
+mkMachine { } ({ pkgs, ... }: {
 
   imports = [
-    #./hardware-configuration.nix
+    ./hardware-configuration.nix
   ];
 
+  boot.loader = {
+    timeout = 5;
+    systemd-boot = {
+      enable = true;
+      editor = false;
+    };
+    efi.canTouchEfiVariables = true;
+  };
+
+  users = {
+    mutableUsers = false;
+    users = {
+      mmarx = {
+        isNormalUser = true;
+        createHome = true;
+        extraGroups = [ "wheel" ];
+        group = "users";
+        home = "/home/mmarx";
+        hashedPassword = "$6$rounds=424242$If29MAgIOTOY9$mi2kyooy/lzIR6F9YPQo0bnkfjpBIdFswHbwgn2yxfqAnOwgni7TLGGk2HCUldG0T1Z7Qu9mhYaNdm0EJkJl6.";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB8FCFThGOBFw6kGprgqlLU6bylvanxmZtgBUAS2sJcT mmarx@korenchkin"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICM97cpt3/r5P8qD5j5zk3XOs3NJO4tTQPAqef33xBYL mmarx@delacroix"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQAh9/kfa4v5102PhJ4EBqFS5aTjbYTgPaGAr6lJ9Hs mmarx@bloome"
+        ];
+      };
+
+      beinke = {
+        isNormalUser = true;
+        createHome = true;
+        extraGroups = [ "wheel" ];
+        group = "users";
+        home = "/home/beinke";
+        hashedPassword = "$6$rounds=424242$4XeOOipFMr154yFt$duKTFu2mSR9LnrGILjgumlxl8FltvCo9RBjhWi1N56avEVaAJym3LFlw3y2.JMCVYAO2ZpK75eF7B/7cSu5rR0";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMCC4cFL1xcZOsIzXg1b/M4b89ofMKErNhg9s+0NdBVC beinke@th1"
+        ];
+      };
+    };
+  };
+
+  networking = {
+    hostName = "brausefrosch";
+    interfaces."enp1s0".ipv4 = {
+      addresses = [{
+        address = "141.30.30.154";
+        prefixLength = 25;
+      }];
+      routes = [{
+        address = "0.0.0.0";
+        prefixLength = 0;
+        via = "141.30.30.129";
+      }];
+    };
+  };
+
+  services.sshd.enable = true;
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
 })
