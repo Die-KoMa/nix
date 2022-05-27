8424c2b226b59c180a4c2a901377fe4f7b8bf3c6
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2022-05-27 12:15:23 +0200

    Add KoMapedia module
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
 
     nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
 
+    komapedia = {
+      url = github:Die-KoMa/mediawiki;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     wat = {
       url = github:thelegy/wat;
       inputs.nixpkgs.follows = "nixpkgs";
@@ -20,11 +25,15 @@
   outputs = flakes@{ wat, nixpkgs, sops-nix, ... }: wat.lib.mkWatRepo flakes ({ findModules, findMachines, ... }: {
     loadModules = [
       flakes.sops-nix.nixosModules.sops
+    ] ++ (nixpkgs.lib.attrValues flakes.komapedia.nixosModules);
+    loadOverlays = [
+      flakes.komapedia.overlay
     ];
-    outputs = (wat.lib.withPkgsForLinux nixpkgs [ flakes.sops-nix.overlay ] (pkgs: {
-      devShell = import ./secrets/shell.nix { inherit pkgs; };
-
-    })) // {
+    outputs = {
+      devShell = wat.lib.withPkgsFor [ "x86_64-linux" ] nixpkgs [ flakes.sops-nix.overlay ]
+        (pkgs:
+          import ./secrets/shell.nix { inherit pkgs; }
+        );
 
       nixosModules = findModules [ "KoMa" ] ./modules;
 
--- modules/komapedia.nix
+++ modules/komapedia.nix
@@ -3,11 +3,12 @@
 mkTrivialModule {
   die-koma.komapedia.enable = true;
 
-  sops.secrets.komapedia-secret-key = {
+  sops.secrets.secret-key = {
     mode = "0400";
     owner = "mediawiki";
     group = config.services.nginx.group;
     path = "/var/lib/mediawiki/secret.key";
+    sopsFile = ../secrets/komapedia.yml;
   };
 
 }
