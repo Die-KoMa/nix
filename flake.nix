f43890d7f869d83c53af23304f532f5d708075b3
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2022-08-28 20:28:37 +0200

    Bump flake

--- flake.nix
+++ flake.nix
@@ -2,45 +2,40 @@
 
   inputs = {
 
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
     komapedia = {
-      url = github:Die-KoMa/mediawiki;
+      url = "github:Die-KoMa/mediawiki";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     wat = {
-      url = github:thelegy/wat;
+      url = "github:thelegy/wat";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     sops-nix = {
-      url = github:Mic92/sops-nix;
+      url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
   };
 
+  outputs = flakes@{ wat, nixpkgs, sops-nix, ... }:
+    wat.lib.mkWatRepo flakes ({ findModules, findMachines, ... }: {
+      loadModules = [ flakes.sops-nix.nixosModules.sops ]
+        ++ (nixpkgs.lib.attrValues flakes.komapedia.nixosModules);
+      loadOverlays = [ flakes.komapedia.overlay ];
+      outputs = {
+        devShell = wat.lib.withPkgsFor [ "x86_64-linux" ] nixpkgs
+          [ flakes.sops-nix.overlay ]
+          (pkgs: import ./secrets/shell.nix { inherit pkgs; });
 
-  outputs = flakes@{ wat, nixpkgs, sops-nix, ... }: wat.lib.mkWatRepo flakes ({ findModules, findMachines, ... }: {
-    loadModules = [
-      flakes.sops-nix.nixosModules.sops
-    ] ++ (nixpkgs.lib.attrValues flakes.komapedia.nixosModules);
-    loadOverlays = [
-      flakes.komapedia.overlay
-    ];
-    outputs = {
-      devShell = wat.lib.withPkgsFor [ "x86_64-linux" ] nixpkgs [ flakes.sops-nix.overlay ]
-        (pkgs:
-          import ./secrets/shell.nix { inherit pkgs; }
-        );
+        nixosModules = findModules [ "KoMa" ] ./modules;
 
-      nixosModules = findModules [ "KoMa" ] ./modules;
+        nixosConfigurations = findMachines ./machines;
 
-      nixosConfigurations = findMachines ./machines;
-
-    };
-  });
-
+      };
+    });
 
 }
