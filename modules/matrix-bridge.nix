577ab9541ecb60557a5eb5d0cf8ec3297a715fed
Dennis Frieberg <nerfingen@yahoo.de>
2022-11-01 17:58:25 +0100

    first attempt of working matrix-bridge module

--- modules/matrix-bridge.nix
+++ modules/matrix-bridge.nix
@@ -6,19 +6,31 @@ mkModule {
   options = cfg:
     liftToNamespace {
       serverName = mkOption {
-        description = "Server name for the synapse server";
+        description = "Server name for the synapse server (the actual fqdn of the server)";
         type = types.str;
       };
 
       domain = mkOption {
-        description = "domain name for the synapse server";
+        description = "domain name for the synapse server, this is what appears in room and user id. It only needs to host .well-known";
         type = types.str;
       };
 
       port = mkOption {
-        description = "Port number to listen on";
+        description = "Port number for the homeserver to listen on";
         type = types.port;
         default = 8008;
+      };
+      
+      bridgePort = mkOption {
+        description = "Port number for the bridge to listen on";
+        type = types.port;
+        default = 29317;
+      };
+
+      bridgeAdmin = mkOption {
+        description = "The name of the admin matrix user for the bridge";
+        type = types.str;
+        default = "telegramAdmin";
       };
     };
 
@@ -36,6 +48,9 @@ mkModule {
         add_header Access-Control-Allow-Origin *;
         return 200 '${builtins.toJSON data}';
       '';
+
+      bridgeUser = "mautrix-telegram";
+      bridgeGroup = "mautrix-telegram";
     in {
       networking.firewall.allowedTCPPorts = [ 80 443 ];
 
@@ -43,7 +58,7 @@ mkModule {
         matrix-synapse = {
           enable = true;
           settings = {
-            server_name = cfg.serverName;
+            server_name = cfg.domain;
             enable_registration = false;
 
             listeners = [{
@@ -58,6 +73,11 @@ mkModule {
                 compress = true;
               }];
             }];
+            app_service_config_files = [
+              # This file needs to be copied from /var/lib/mautrix-telegram/telegram-regestration.yaml
+              # and the access rights needs to be fixed.
+              "/var/lib/matrix-synapse/telegram-registration.yaml"
+          ];
           };
         };
 
@@ -70,6 +90,13 @@ mkModule {
               LC_COLLATE = "C"
               LC_CTYPE = "C";
           '';
+          ensureDatabases = [ "mautrix-telegram" ];
+          ensureUsers = [
+            {
+              name = bridgeUser;
+              ensurePermissions = { "DATABASE mautrix-telegram" = "ALL PRIVILEGES";};
+            }
+          ];
         };
 
         nginx = {
@@ -101,14 +128,58 @@ mkModule {
             };
           };
         };
-
         mautrix-telegram = {
           enable = true;
           environmentFile = config.sops.secrets.mautrix-env-file.path;
           settings = {
-            # TODO
+            homeserver = {
+              # we could also go for https:// on port 443 and talk through nginx, but I don't
+              # see any benefit in this for local communication
+              address = "http://localhost:${toString cfg.port}";
+              domain = "cfg.domain";
+            };
+            appservice = {
+              address = "http://localhost:${toString cfg.bridgePort}";
+              tls_cert = false;
+              tls_key = false;
+              port = cfg.bridgePort;
+              provisioning.enable = false;
+              public.enable = false;
+              id = "telegram";
+              bot_username = "telegrambot";
+              bot_displayname = "Telegram bridge bot";
+              database = "postgres://localhost/mautrix-telegram";
+            };
+            bridge = {
+              authless_portals = false;
+              permissions = {
+                "*" = "relaybot";
+                "@${cfg.bridgeAdmin}:${cfg.domain}" = "admin";
+              };
+              animated_sticker = {
+                target = "gif";
+                convert_from_webm = false;
+                args = {
+                  width = 256;
+                  height = 256;
+                  fs = 25;
+                };
+              };
+            };
           };
         };
+      };
+
+      systemd.services.mautrix-telegram = {
+        serviceConfig = {
+          User = bridgeUser;
+          Group = bridgeGroup;
+        
+        };
+
+        path = with pkgs; [
+          lottieconverter # sadly unfree :( (tool to convert animated stickers)
+        ];
       };
 
       sops.secrets.mautrix-env-file = {
