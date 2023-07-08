ff7d8004be11610889b1c2b8b9554c343062adcf
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-07-08 21:40:59 +0200

    Add KoMacloud shortlinks

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -1,4 +1,3 @@
-
 { mkTrivialModule, config, pkgs, lib, ... }:
 
 with lib;
@@ -7,6 +6,35 @@ let
   domainName = "cloud.die-koma.org";
   extraDomainNames = [ "51.cloud.die-koma.org" ];
   home = "/data/nextcloud";
+  shortlinks = {
+    public = "/s/Li2bQd4q5p8NMCR";
+    koma61 = "/s/k4dcMGq5dEqJqLB";
+    koma64 = "/s/rMQfoRsQSwmEwCX";
+    koma65 = "/s/jFTksRPfY94pF78";
+    koma66 = "/s/H5kxttxd26YiaSQ";
+    koma67 = "/s/5DztHrxry6tfxzo";
+    koma68 = "/s/LfFiEGp7xjQsmqE";
+    koma69 = "/s/XNGCTBj5gifNS45";
+    koma70 = "/s/EatMniZNFfCNZLj";
+    koma71 = "/s/73eSmezMFLZxSSB";
+    koma72 = "/s/QCYaJ6fW6kfzfDz";
+    koma73 = "/s/DSGxY43LwBy3kbP";
+    koma74 = "/s/GWYLCEMRgiDiQpD";
+    koma75 = "/s/QCwbNkst4CiRMWG";
+    koma76 = "/s/TFezieeJzsK3knM";
+    koma77 = "/s/ACR2bGy8YgSsCWa";
+    koma78 = "/s/oCfeqKJ26tcJeEq";
+    koma79 = "/s/tRNcgNGyzNeA9Wf";
+    koma80 = "/s/a96eaNFyT77rNgH";
+    koma81 = "/s/XYwsLGXTDJzZCw7";
+    koma82 = "/s/zWb6fCRxwbYE7Zj";
+    koma83 = "/s/sxdwt5BbB2Tjo4L";
+    koma84 = "/s/fqZFxSFiJGqMZF6";
+    koma85 = "/s/dNjXwPRSiEFeJJY";
+    koma86 = "/s/4gFEg854HSJoZtf";
+    koma87 = "/s/386rSxrDjp36aL6";
+    koma88 = "/s/wP5FJkrG93pZsnY";
+  };
 in mkTrivialModule {
 
   wat.KoMa = {
@@ -27,7 +55,7 @@ in mkTrivialModule {
 
   services.redis = {
     vmOverCommit = true;
-    servers.nextcloud.save = [];
+    servers.nextcloud.save = [ ];
   };
 
   services.nextcloud = {
@@ -52,6 +80,10 @@ in mkTrivialModule {
     forceSSL = true;
     useACMEHost = config.wat.KoMa.nginx.useACMEHost;
     serverAliases = extraDomainNames;
+    locations = mapAttrs' (link: target:
+      nameValuePair "/${link}" {
+        return = "301 https://${domainName}${target}";
+      }) shortlinks;
   };
 
 }
