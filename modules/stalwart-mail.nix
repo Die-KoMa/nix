e4cc29977c1a97451cfdfe45f300e8d2f6955422
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2024-11-30 18:31:16 +0100

    Add stalwart-mail

---
+++ modules/stalwart-mail.nix
@@ -0,0 +1,93 @@
+{
+  mkModule,
+  config,
+  pkgs,
+  lib,
+  liftToNamespace,
+  ...
+}:
+
+with lib;
+
+mkModule {
+  options =
+    cfg:
+    liftToNamespace {
+      managementPort = mkOption {
+        description = "Port number for the management to listen on";
+        type = types.port;
+        default = 8009;
+      };
+
+      domains = mkOption {
+        description = "domain names for the mail server";
+        type = types.listOf types.str;
+      };
+    };
+
+  config =
+    cfg:
+    let
+      adminSecret = "stalwart-mail-admin";
+      managementURL = "[::1]:${toString cfg.managementPort}";
+    in
+    {
+      services.stalwart-mail = {
+        enable = true;
+        openFirewall = true;
+        settings = {
+          server = {
+            hostname = config.networking.fqdn;
+            listener = {
+              smtp = {
+                protocol = "smtp";
+                bind = "[::]:25";
+              };
+              submission = {
+                protocol = "smtp";
+                bind = "[::]:465";
+              };
+              imaps = {
+                protocol = "imap";
+                bind = "[::]:993";
+              };
+              management = {
+                bind = [ managementURL ];
+                protocol = "http";
+              };
+            };
+          };
+          lookup.default.hostname = config.networking.fqdn;
+          certificate.default =
+            let
+              acmePath = name: "%{file:/var/lib/acme/${config.wat.KoMa.acme.defaultCertName}/${name}}%";
+            in
+            {
+              cert = acmePath "cert.pem";
+              private-key = acmePath "key.pem";
+              default = true;
+            };
+          authentication.fallback-admin = {
+            user = "admin";
+            secret = "%{file:${config.sops.secrets.${adminSecret}.path}}%";
+          };
+        };
+      };
+
+      sops.secrets.${adminSecret} = {
+        format = "yaml";
+        mode = "0600";
+        owner = "stalwart-mail";
+      };
+
+      systemd.services.stalwart-mail.reload = ''
+        export URL=https://${managementURL}
+        export CREDENTIALS=$(cat ${config.sops.secrets.${adminSecret}.path})
+        stalwart-cli server reload-config
+        stalwart-cli server reload-certificates
+      '';
+
+      wat.KoMa.acme.reloadUnits = [ "stalwart-mail.service" ];
+
+    };
+}
