dd42897d804954ca661afad4d9d9124556a3dfb3
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-06-15 04:02:39 +0200

    Add aksync

--- machines/brausefrosch/default.nix
+++ machines/brausefrosch/default.nix
@@ -30,6 +30,13 @@ mkMachine { } (
       };
       base.enable = true;
       grafana-cloud.enable = true;
+      aksync = {
+        enable = true;
+        onCalendar = [
+          "2025-06-15..23 *:0/5:00"
+          "2025-06-24..31 *:0/15:00"
+        ];
+      };
       homepage.enable = true;
       komapedia.enable = true;
       nextcloud.enable = true;
