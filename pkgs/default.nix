19220d64a01b5025b25d6f8ff2692f5876f3099c
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-08-12 20:45:32 +0200

    Upgrade matrix for security issue

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,3 +1,18 @@
-flakes: final: prev: with final; {
+flakes: final: prev:
+let
+  inherit (prev) lib system;
+  inherit (lib) versionOlder getVersion mapAttrs;
+  unstable = flakes.nixpkgs-unstable.legacyPackages.${system};
 
+  atLeastOrOverride =
+    from: pkg: version:
+    if versionOlder (getVersion prev.${pkg}) version then from.${pkg} else prev.${pkg};
+  overrideOlder' = from: mapAttrs (atLeastOrOverride from);
+  overrideOlder = overrideOlder' unstable;
+in
+(overrideOlder {
+  element-desktop = "1.11.109";
+  matrix-synapse-unwrapped = "1.135.2";
+})
+// {
 }
