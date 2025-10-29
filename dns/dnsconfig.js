fd8af7978ade3031f1005ebcb159f3b9ddac65dc
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-10-29 19:25:20 +0100

    Add CNAME for aks.die-koma.org

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -44,11 +44,11 @@ function HOST(record_name, host, options) {
 }
 
 function CAA_LETSENCRYPT(record_name) {
-  return CAA_BUILDER({
-    label: record_name || "@",
-    issue: ["letsencrypt.org;validationmethods=dns-01"],
-    issue_critical: true,
-  });
+    return CAA_BUILDER({
+        label: record_name || "@",
+        issue: ["letsencrypt.org;validationmethods=dns-01"],
+        issue_critical: true,
+    });
 }
 
 DEFAULTS(NAMESERVER_TTL("1d"), DefaultTTL("1h"), []);
@@ -59,7 +59,7 @@ D(
     DnsProvider("he"),
     NAMESERVER_TTL("2d"),
     IGNORE("_acme-challenge.*", "TXT"),
-    []
+    [],
 );
 
 D(
@@ -70,7 +70,7 @@ D(
     IGNORE("_acme-challenge.*", "TXT"),
     // we need _some_ record here so that lego doesn't fail because it receives an NXDOMAIN
     A("_acme-challenge.brausefrosch", "127.0.0.1"),
-    []
+    [],
 );
 
 D(
@@ -91,9 +91,18 @@ D(
 
     MX("@", 10, "brausefrosch.hosts.die-koma.org.", TTL(86400)),
     TXT("@", "v=spf1 mx ra=postmaster -all"),
-    TXT("_dmarc", "v=DMARC1; p=reject; rua=mailto:postmaster@die-koma.org; ruf=mailto:postmaster@die-koma.org"),
-    TXT("202503e._domainkey", "v=DKIM1; k=ed25519; h=sha256; p=Sn+LyM14oE9CkikTZuKldwT9Xo5aiwFbB+VIMfGZPjI="),
-    TXT("202503r._domainkey", "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxMZ8aB44058PE1TVurqo7oKSxJTweFkJAwdYwsZpicD44jVJ9PK1MkDa3kYpl016Ykv0azDK+cH4DTvuwE7T0rw/qXJEAQpqsmkS6kGQWwnPDJDNJZisEG0PlMbk+HM+C301Pv+7St8alNfyIK0ckfuNyf03h4gnfv4UQCd5GLYNlIqZRbmvwSyGSwkeqPlXT6v3ohZQ3vL0o0I6s+ArCc379f1Mxv5NiwpYgTyHRab7RmRJeWAD2kafrjNhAWWAAcibsFxVE8ZtysbF2m2NRAcraYWPZBu9bXFajpbapzqrrH55aV0T8DFnf3yPkcmAgwpm6RjS5XvUHKfD58sUvQIDAQAB"),
+    TXT(
+        "_dmarc",
+        "v=DMARC1; p=reject; rua=mailto:postmaster@die-koma.org; ruf=mailto:postmaster@die-koma.org",
+    ),
+    TXT(
+        "202503e._domainkey",
+        "v=DKIM1; k=ed25519; h=sha256; p=Sn+LyM14oE9CkikTZuKldwT9Xo5aiwFbB+VIMfGZPjI=",
+    ),
+    TXT(
+        "202503r._domainkey",
+        "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxMZ8aB44058PE1TVurqo7oKSxJTweFkJAwdYwsZpicD44jVJ9PK1MkDa3kYpl016Ykv0azDK+cH4DTvuwE7T0rw/qXJEAQpqsmkS6kGQWwnPDJDNJZisEG0PlMbk+HM+C301Pv+7St8alNfyIK0ckfuNyf03h4gnfv4UQCd5GLYNlIqZRbmvwSyGSwkeqPlXT6v3ohZQ3vL0o0I6s+ArCc379f1Mxv5NiwpYgTyHRab7RmRJeWAD2kafrjNhAWWAAcibsFxVE8ZtysbF2m2NRAcraYWPZBu9bXFajpbapzqrrH55aV0T8DFnf3yPkcmAgwpm6RjS5XvUHKfD58sUvQIDAQAB",
+    ),
     TXT("_smtp.tls", "v=TLSRPTv1; rua=mailto:postmaster@die-koma.org"),
 
     HOST("@", "brausefrosch"),
@@ -109,8 +118,9 @@ D(
     CNAME_HOST("matrix", "brausefrosch"),
     CNAME_HOST("new", "brausefrosch"),
     CNAME_HOST("www", "brausefrosch"),
+    CNAME_HOST("aks", "brausefrosch"),
 
-    []
+    [],
 );
 
 D(
@@ -125,15 +135,24 @@ D(
 
     MX("@", 10, "brausefrosch.hosts.die-koma.org.", TTL(86400)),
     TXT("@", "v=spf1 mx ra=postmaster -all"),
-    TXT("_dmarc", "v=DMARC1; p=reject; rua=mailto:postmaster@komapedia.org; ruf=mailto:postmaster@komapedia.org"),
-    TXT("202411e._domainkey", "v=DKIM1; k=ed25519; h=sha256; p=kPe7mzLvckTRpvIKugKJbzpyiZm15ojeSCP0Ko/Oz0w="),
-    TXT("202411r._domainkey", "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyCPH5+cAh6vzGuNeQDfZa/D296+edvyGeb/FGlyg1kJNuafVHEBy4d3Ecpf0nu+kspwdyTxk+hWRmqEV6tkvhtY3e2WryKvaY+0KMQaNZRvaAEF3fJ3sGNwCqFhi7hZe0/wglLKuEup1sWNGGEVH+W+z6zgVnNsxLfY/MoPdR91K7ZK6BFbxqVZbg81tNScOyQFzXrR9T6WVZDrJJw+qQRsqbJqK2TI6CLjzGHsnwAUcc3jZub0ZGPACM/alqlcKknW8UoDnKeVsoMGJJMqjStE9gqwfVXGfrW8EOtggA/0KfrPToKvUzOmLjiDeHUYz0ZEf1p14YCwcLf1yrgkXnQIDAQAB"),
+    TXT(
+        "_dmarc",
+        "v=DMARC1; p=reject; rua=mailto:postmaster@komapedia.org; ruf=mailto:postmaster@komapedia.org",
+    ),
+    TXT(
+        "202411e._domainkey",
+        "v=DKIM1; k=ed25519; h=sha256; p=kPe7mzLvckTRpvIKugKJbzpyiZm15ojeSCP0Ko/Oz0w=",
+    ),
+    TXT(
+        "202411r._domainkey",
+        "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyCPH5+cAh6vzGuNeQDfZa/D296+edvyGeb/FGlyg1kJNuafVHEBy4d3Ecpf0nu+kspwdyTxk+hWRmqEV6tkvhtY3e2WryKvaY+0KMQaNZRvaAEF3fJ3sGNwCqFhi7hZe0/wglLKuEup1sWNGGEVH+W+z6zgVnNsxLfY/MoPdR91K7ZK6BFbxqVZbg81tNScOyQFzXrR9T6WVZDrJJw+qQRsqbJqK2TI6CLjzGHsnwAUcc3jZub0ZGPACM/alqlcKknW8UoDnKeVsoMGJJMqjStE9gqwfVXGfrW8EOtggA/0KfrPToKvUzOmLjiDeHUYz0ZEf1p14YCwcLf1yrgkXnQIDAQAB",
+    ),
     TXT("_smtp.tls", "v=TLSRPTv1; rua=mailto:postmaster@komapedia.org"),
 
     CNAME_HOST("de", "brausefrosch"),
     CNAME_HOST("file", "brausefrosch"),
     CNAME_HOST("www", "brausefrosch"),
-    []
+    [],
 );
 
 // Local Variables:
