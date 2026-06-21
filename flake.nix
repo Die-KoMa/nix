a075a851931d0f13467b8708846c40cbf1364ef6
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2026-06-21 19:08:27 +0200

    Fix typos

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
 
     nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.xz";
-    nixpkgs-unstable.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs-unstable.url = "https://channels.nixos.org/nixos-unstable-small/nixexprs.tar.xz";
 
     komapedia = {
       url = "github:Die-KoMa/mediawiki";
